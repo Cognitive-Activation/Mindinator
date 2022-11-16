af3e974902f88bbe4ac4f41ac67900daf3c856bd
Vinit-Tiwari <95700013+Vinit-Tiwari@users.noreply.github.com>
2022-11-16 10:29:59 +0530

    Updated home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,44 @@
-<script>
-    import Navbar from '../components/Navbar.svelte'
-    import '../styles/jura-font.css'
-</script>
+<span class="navbar-container row gap1">
+  <span class="name row gap1">
+    <span> logo </span>
 
-<Navbar />
-<slot />
+    <span> Mindinator </span>
+  </span>
+
+  <span class="menu-options row  gap1">
+    <p>Dashboard</p>
+
+    <p>Blog</p>
+
+    <p>Riddles</p>
+
+    <p>Sign in</p>
+  </span>
+</span>
+
+<style>
+  .row {
+    display: flex;
+    align-items: center;
+  }
+  .column {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  .gap1 {
+    gap: 1rem;
+  }
+  .navbar-container {
+    padding: 2rem;
+    justify-content: space-between;
+  }
+  .menu-options {
+    justify-content: space-around;
+    width: 100%;
+    max-width: 45rem;
+  }
+  .menu-options > p {
+    cursor: pointer;
+  }
+</style>
