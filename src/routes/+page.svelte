a5f83d36fedbad992c31a4ff51c13a2e4d06c059
Vinit-Tiwari <95700013+Vinit-Tiwari@users.noreply.github.com>
2022-11-16 10:27:55 +0530

    Updated home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,104 @@
-<script>
-    import SEO from "../components/SEO.svelte";
-</script>
+<span class="home-container row">
+  <span class="home-page-content column">
+    <p class="title">Mindinator</p>
 
-<SEO title="Brain games" description="Games that will train your brain." />
+    <p class="desc">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </p>
 
-<div class="main">
-    <h1>MINDINATOR</h1>
-    <h2><a class="start" href="./games">Start</a></h2>
-</div>
+    <p class="start-button">Start</p>
+  </span>
 
-<p class="desc">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sunt
-    quibusdam laborum provident quo, fugit inventore at, esse, ipsam voluptates
-    dignissimos! Dolorum, sequi hic temporibus dignissimos deleniti quos. Quos,
-    rem?
-</p>
-<p class="desc">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste unde pariatur
-    possimus sapiente facilis quam a saepe placeat assumenda sunt aperiam, omnis
-    nesciunt temporibus labore itaque nihil! Hic, harum aspernatur!
-</p>
+  <span>
+    <img class="brain-img" src="brain-img.png" alt="">
+  </span>
+</span>
 
 <style>
-    @font-face {
-        font-family: "Jura";
-        font-style: normal;
-        font-weight: 400;
-        src: url("./fonts/Jura-Regular.ttf");
-    }
-
-    .start {
-        background-color: #1b1a1a;
-        color: #ffffff;
-        padding: 0.7em 1.4em;
-        border-radius: 0.5em;
-        text-decoration: none;
-        font-size: larger;
-    }
-    .main {
-        font-family: "Jura";
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        align-items: center;
-        justify-content: center;
-        height: 50vh;
-    }
-
-    .desc {
-        padding-left: 10%;
-        padding-right: 10%;
-    }
-
-    button {
-        background-color: black;
-        color: white;
-        padding-top: 1em;
-        padding-bottom: 1em;
-        padding-left: 3em;
-        padding-right: 3em;
-    }
+  :global(body) {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    /* background-color: #222222; */
+    color: white;
+    background: radial-gradient(
+      50% 50% at 50% 50%,
+      rgba(44, 44, 44, 0.78) 0%,
+      #2c2c2c 100%
+    );
+  }
+  :global(*) {
+    margin: 0%;
+    padding: 0%;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+  }
+  :global(a){
+    text-decoration: none;
+    color:white;
+  }
+  :global(a:link(#exclude), a:visited(#exclude) ){
+    text-decoration: none;
+    color: blue;
+  }
+  :global(.row) {
+    display: flex;
+    align-items: center;
+  }
+  :global(.column) {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  .home-container {
+    padding:5rem 5rem;
+    align-items: center;
+    max-height: 40rem;
+    justify-content: space-between;
+    gap: 1rem;
+  }
+  .home-page-content {
+    max-width: 650px;
+    width: 100%;
+    height: 25rem;
+    justify-content: space-evenly;
+    align-items: flex-start;
+  }
+  .title {
+    font-size: 2.5rem;
+    font-weight: bold;
+  }
+  .start-button {
+    display: flex;
+    height: 2rem;
+    background: linear-gradient(
+      97.77deg,
+      #41aaf5 -5.92%,
+      #f56387 49.06%,
+      #41aaf5 98%
+    );
+    align-items: center;
+    justify-content: center;
+    padding: 1.5rem 2.5rem;
+    font-size: 1.5rem;
+    font-weight: 200;
+    border-radius: 15px;
+    cursor: pointer;
+    background-size: 300% 100%;
+    -moz-transition: all 0.4s ease-in-out;
+    -o-transition: all 0.4s ease-in-out;
+    -webkit-transition: all 0.4s ease-in-out;
+    transition: all 0.4s ease-in-out;
+  }
+  .start-button:hover {
+    background-position: 100% 0;
+    -moz-transition: all 0.4s ease-in-out;
+    -o-transition: all 0.4s ease-in-out;
+    -webkit-transition: all 0.4s ease-in-out;
+    transition: all 0.4s ease-in-out;
+  }
 </style>
