2ebc52373e8440f239f18aeebb206923854696f4
SadCatto <rushimalu44@gmail.com>
2023-04-13 19:06:29 +0530

    Updated terms and conditions

---
+++ src/routes/cookies/+page.svelte
@@ -0,0 +1,35 @@
+<main>
+    <h1>Cookies Policy for Mindinator.com</h1>
+    <p>Last updated: April 9, 2023</p>
+
+    <p>This Cookies Policy explains how Mindinator.com ("we," "us," or "our") uses cookies and similar technologies when you visit our website. By using our website and agreeing to this Cookies Policy, you consent to our use of cookies in accordance with the terms of this policy.</p>
+
+    <h2>What are cookies?</h2>
+    <p>Cookies are small text files that are stored on your computer or mobile device when you visit a website. They help websites remember your preferences, track your online activity, and improve your browsing experience.</p>
+
+    <h2>How we use cookies</h2>
+    <p>We use cookies for the following purposes:</p>
+    <ul>
+        <li><strong>Authentication:</strong> We use cookies to recognize you when you visit our website and keep you logged in.</li>
+        <li><strong>Personalization:</strong> We use cookies to remember your preferences, such as your preferred language and display settings.</li>
+        <li><strong>Analytics:</strong> We use cookies to collect information about how you use our website, such as the pages you visit and the time you spend on the site. This helps us analyze user behavior and improve our website.</li>
+        <li><strong>Marketing:</strong> We use cookies to display relevant advertisements and track their effectiveness.</li>
+    </ul>
+
+    <h2>Third-party cookies</h2>
+    <p>In addition to our own cookies, we may also use various third-party cookies to report usage statistics, deliver advertisements, and enable social media sharing. These third-party cookies are governed by their respective privacy policies, and we encourage you to review them.</p>
+
+    <h2>Managing cookies</h2>
+    <p>Most web browsers allow you to manage cookies through their settings. You can choose to block or delete cookies, but this may negatively affect your browsing experience on our website. To learn more about how to manage cookies, consult your browser's help documentation or visit <a href="https://www.allaboutcookies.org/" target="_blank" rel="noopener noreferrer">www.allaboutcookies.org</a>.</p>
+
+    <h2>Changes to this Cookies Policy</h2>
+    <p>We may update this Cookies Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. Your continued use of our website constitutes your acceptance of the updated policy.</p>
+
+    <h2>Contact us</h2>
+    <p>If you have any questions or concerns about this Cookies Policy, please contact us at:</p>
+    <!-- Add your contact information here -->
+</main>
+
+<footer>
+    <!-- Add your website's footer here -->
+</footer>
