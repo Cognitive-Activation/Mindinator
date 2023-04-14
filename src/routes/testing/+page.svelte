684666dfb4f464c90017c584da0e62b5bbc2ed4b
sudhanshu-p <pandeysv@rknec.edu>
2023-04-14 10:59:02 +0530

    UI changes in Toggle, added SVG

---
+++ src/routes/testing/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Login from "../../components/Login.svelte";
+    let isOpenModal = false;
+    function openModal() {
+        isOpenModal = true;
+    }
+
+    function closeModal() {
+        isOpenModal = false;
+    }
+</script>
+
+<button on:click={openModal}>Open Modal</button>
+<div class="login-box">
+    <Login isOpenModal:isOpenModal on:closeModal={closeModal} />
+</div>
